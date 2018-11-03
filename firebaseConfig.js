85b894ed53a46e0c4aaeaccb84f11f6d5215a662
sayoun94 <38676254+sayoun94@users.noreply.github.com>
2018-11-03 11:55:27 -0700

    Created firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// firebase init goes here
+export default {
+    apiKey: "AIzaSyAtTNO9fCUwweAnOXWbbAaOR39t7X1hdCQ",
+    authDomain: "studybuddy-memo.firebaseapp.com",
+    databaseURL: "https://studybuddy-memo.firebaseio.com",
+    projectId: "studybuddy-memo",
+    storageBucket: "studybuddy-memo.appspot.com",
+    messagingSenderId: "986425946679"
+}
